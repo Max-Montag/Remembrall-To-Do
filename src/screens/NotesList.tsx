669d6c6cc0774320da69a0a1e9e7e95b843fc32a
MaMon94 <max.montag1994@gmail.com>
2023-03-19 21:09:51 +0100

    minor

---
+++ src/screens/NotesList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const NotesList: React.FC = () => {
+  return (
+    <View>
+      <Text>Notes</Text>
+    </View>
+  );
+};
+
+export default NotesList;
