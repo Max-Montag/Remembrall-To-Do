ab71de79299e67961e5a078dcbb5ecde62d304fd
MaMon94 <max.montag1994@gmail.com>
2023-03-19 21:36:49 +0100

    minor

--- App.tsx
+++ App.tsx
@@ -1,24 +1,117 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import notesReducer from './src/reducers/notesReducer';
-import NotesList from './src/screens/NotesList';
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  FlatList,
+} from 'react-native';
 
-const Stack = createStackNavigator();
-const store = createStore(notesReducer);
-
-const App: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Notes" component={NotesList} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-  );
+type Note = {
+  id: number;
+  content: string;
 };
+
+function App(): JSX.Element {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addNote = () => {
+    if (inputValue.trim() !== '') {
+      setNotes([...notes, { id: Date.now(), content: inputValue }]);
+      setInputValue('');
+    }
+  };
+
+  const deleteNote = (id: number) => {
+    setNotes(notes.filter((note) => note.id !== id));
+  };
+
+  const renderItem = ({ item }: { item: Note }) => (
+    <View style={styles.note}>
+      <Text>{item.content}</Text>
+      <TouchableOpacity onPress={() => deleteNote(item.id)}>
+        <Text style={styles.deleteButton}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <View style={styles.header}>
+        <Text style={styles.title}>Awesome Notes</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={inputValue}
+          onChangeText={setInputValue}
+          style={styles.input}
+          placeholder="Write a note..."
+        />
+        <TouchableOpacity onPress={addNote} style={styles.addButton}>
+          <Text style={styles.addButtonText}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={notes}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    padding: 20,
+    backgroundColor: '#6200EE',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: '#6200EE',
+    fontSize: 18,
+    paddingVertical: 10,
+  },
+  addButton: {
+    marginLeft: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    backgroundColor: '#6200EE',
+    borderRadius: 5,
+  },
+  addButtonText: {
+    fontSize: 18,
+    color: 'white',
+  },
+  note: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  },
+  deleteButton: {
+    color: 'red',
+  },
+});
 
 export default App;
